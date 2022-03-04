1ad6e36ef3e63d7b86a4113eca1d784475f106d6
stanescon <stanescon.n@gmail.com>
2022-03-04 14:20:00 -0300

    adicionando comandos do input select

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
-const cartoes = document.querySelectorAll('.cartao-pais')
+const cartoes = document.querySelectorAll('.cartao-pais');
 const listaPaisesPrincipais = ['germany', 'united', 'brazil', 'iceland', 'mexico', 'aland', 'albania', 'algeria']
+const conteudoPrincipal = document.querySelector('.conteudo-principal');
+const conteudoAleatorio = document.querySelector('.conteudo-aleatorio');
+const conteudoDinamico = document.querySelector('.conteudo-dinamico');
+
+
+
 
 function incluirPaisesPrincipais(pais, posicao) {
     fetch(`https://restcountries.com/v3.1/name/${pais}`)
@@ -27,31 +33,25 @@ for(let i=0; i < cartoes.length; i++){
 }
 
 
-function adcElemento (string) {
+function adcElemento (divPai, indiceMarcacao, string) {
     const divNova = document.createElement("div");
     divNova.innerHTML = string
     divNova.classList.add('cartao-pais');
-    const conteudoAleatorio = document.querySelector('.conteudo-aleatorio')
-    const divAtual = document.querySelector('.marcacao');
-    conteudoAleatorio.insertBefore(divNova, divAtual);
+    const divAtual = document.querySelector('.marcacao')[indiceMarcacao];
+    divPai.insertBefore(divNova, divAtual);
 }
 
-let bandeira;
-let nome;
-let populacao;
-let capital;
 
-
-function outrosPaises (indice){
+function todosOsPaises (indice){
     fetch('https://restcountries.com/v3.1/all')
     .then(resposta => resposta.json()).then(resposta => {
-        bandeira = resposta[indice].flags.svg
-        nome = resposta[indice].name.common
-        populacao = resposta[indice].population
-        regiao = resposta[indice].region;
-        capital = resposta[indice].capital[0]
-        console.log(bandeira, nome, populacao, regiao, capital)
-    adcElemento(`<img src="${bandeira}" class="bandeira">
+        let bandeira = resposta[indice].flags.svg
+        let nome = resposta[indice].name.common
+        let populacao = resposta[indice].population
+        let regiao = resposta[indice].region;
+        let capital = resposta[indice].capital[0]
+        // console.log(bandeira, nome, populacao, regiao, capital)
+    adcElemento(conteudoAleatorio, 0, `<img src="${bandeira}" class="bandeira">
     <h2 class="titulo">${nome}</h2>
     <p class="populacao"><span>Population:</span> ${populacao}</p>
     <p class="continente"><span>Region:</span> ${regiao}</p>
@@ -61,11 +61,59 @@ function outrosPaises (indice){
 
 
 for(let i=0; i<250; i++){
-    outrosPaises(i)
+    todosOsPaises(i)
 }
 
 
+const filtro = document.querySelector('.filtro')
+
+filtro.addEventListener('change', function(ev){
+    console.log(ev.target.value)
+    let valor = ev.target.value
+    if(valor == 1){
+        mostrarRegiao('africa')
+    } else if(valor == 2){
+        mostrarRegiao('america')
+    } else if(valor == 3){
+        mostrarRegiao('asia')
+    } else if(valor == 4){
+        mostrarRegiao('europe')
+    } else if(valor == 5){
+        mostrarRegiao('oceania')
+    } else if(valor == 0){
+        conteudoDinamico.innerHTML = '<div class="marcacao oculto"></div>'
+        conteudoPrincipal.classList.add('oculto');
+        if(conteudoAleatorio.classList.contains('oculto')){
+            conteudoAleatorio.classList.remove('oculto');
+        }
+    }
+})
 
 
+function mostrarRegiao(regiao){
+    conteudoPrincipal.classList.add('oculto')
+    conteudoAleatorio.classList.add('oculto')
+    conteudoDinamico.innerHTML = '<div class="marcacao oculto"></div>'
+
+    fetch(`https://restcountries.com/v3.1/region/${regiao}`)
+    .then(resposta => resposta.json()).then(resposta => {
+        console.log(resposta)
+    for(let i=0; i < resposta.length; i++){
+        let bandeira = resposta[i].flags.svg
+        let nome = resposta[i].name.common
+        let populacao = resposta[i].population
+        let regiao = resposta[i].region;
+        let capital = ""
+        if(typeof resposta[i].capital[0] == "string"){
+            capital = resposta[i].capital[0]
+        }
+            adcElemento(conteudoDinamico, 1, `<img src="${bandeira}" class="bandeira">
+            <h2 class="titulo">${nome}</h2>
+            <p class="populacao"><span>Population:</span> ${populacao}</p>
+            <p class="continente"><span>Region:</span> ${regiao}</p>
+            <p class="capital"><span>Capital:</span> ${capital}</p>`)   
+    }
+    })
+}
 
 
